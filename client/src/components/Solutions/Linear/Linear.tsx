03d0cf5cf8b440851abfdc05e6047e35b7948a66
Natchkung <natchnon654321@gmail.com>
2024-09-25 16:43:41 +0700

    update matrix

--- client/src/components/Solutions/Linear/Linear.tsx
+++ client/src/components/Solutions/Linear/Linear.tsx
@@ -1,20 +1,29 @@
-import TableMatrix from '../../ui/TableMatrix'
-import { useState } from 'react'
+import { useState } from "react";
+import TableMatrix from "../../ui/TableMatrix";
 
 function Linear() {
-  const [countMatrix, setCountMatrix] = useState(3)
+  const [countMatrix, setCountMatrix] = useState(3);
 
   const handleSetCountMatrix = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCountMatrix(e.target.value as unknown as number );
+    setCountMatrix(e.target.value as unknown as number);
   };
-  
+
   return (
-    <div className='w-full flex justify-center items-center flex-col'>
-      <input type="number" className='mt-10 rounded-md px-5 py-2 w-[200px] text-center' placeholder='3' value={countMatrix}
-      onInput={handleSetCountMatrix}/>
-      <TableMatrix count={countMatrix}/>
+    <div className="w-full flex justify-center items-center flex-col">
+      <input
+        type="number"
+        className="mt-10 rounded-md px-5 py-2 w-[200px] text-center"
+        placeholder="3"
+        value={countMatrix}
+        onChange={handleSetCountMatrix}
+      />
+      <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <TableMatrix count={countMatrix} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Linear
+export default Linear;
--- client/src/components/ui/TableMatrix.tsx
+++ client/src/components/ui/TableMatrix.tsx
@@ -1,10 +1,14 @@
 import {useEffect,useState} from 'react'
+import { BlockMath } from "react-katex";
 
 function TableMatrix(props : {count : number}) {
-    const [Tablematrix, setTableMatrix] = useState<JSX.Element[]>([]);
-    const [matrix, setMatrix] = useState<number[][]>([]);
+    const [TablematrixA, setTableMatrixA] = useState<JSX.Element[]>([]);
+    const [TableArrX, setTableArrX] = useState<JSX.Element[]>([]);
+    const [TableArrB, setTableArrB] = useState<JSX.Element[]>([]);
+    const [matrixA, setMatrixA] = useState<number[][]>([]);
+    const [arrB, setArrB] = useState<number[]>([]);
 
-    const createMatrix = (matrixSize: number) => {
+    const createMatrix= (matrixSize: number) => {
         const matrix = new Array(Number(matrixSize)).fill(0);
         for (let i = 0; i < matrixSize; i++) {
             matrix[i] = new Array(Number(matrixSize)).fill(0);
@@ -12,34 +16,82 @@ function TableMatrix(props : {count : number}) {
         return matrix;
     };
     
-    const createTableMatrix = async (count : number) => {
-        await setTableMatrix([]);
-        await setMatrix(createMatrix(count));
+    const createTableMatrixA = async (count : number) => {
+        await setTableMatrixA([]);
+        await setMatrixA(createMatrix(count));
         if(count > 0 && count <= 10){
             setTimeout(() => {
                 const tempTablematrix = [];
                 for (let i = 0; i < count; i++) {
                     const col = [];
                     for (let j = 0; j < count ; j++) {
-                        col.push(<input type="number" className='w-[70px] h-[70px] text-center rounded-md' key={j} placeholder={`a${i+1}${j+1}`} onInput={(event : React.ChangeEvent<HTMLInputElement> ) => onInputMartrix(event, i, j)}/>);
+                        col.push(<input type="number" className='w-[70px] h-[70px] text-center rounded-md' key={j} placeholder={`a${i+1}${j+1}`} onInput={(event : React.ChangeEvent<HTMLInputElement> ) => onInputMartrixA(event, i, j)}/>);
                     }
                     tempTablematrix.push(<div className='flex justify-center items-center gap-2 mt-2' key={i}>{col}</div>);
                 }
-                setTableMatrix(tempTablematrix);
+                setTableMatrixA(tempTablematrix);
             }, 0.001);
         }
     };
+    const createArrX = async(count : number)=>{
+        await setTableArrX([]);
+        console.log(arrB);
+        if(count > 0 && count <= 10){
+            setTimeout(() => {
+                const tempArr = [];
+                for (let i = 0; i < count; i++) {
+                    tempArr.push(<input type="number" className='w-[70px] h-[70px] text-center rounded-md' disabled key={i} placeholder={`x${i+1}`}/>);
+                }
+                setTableArrX(tempArr);
+            }, 0.001);
+        }
+    
+    }
+    
 
-    const onInputMartrix = (event: React.ChangeEvent<HTMLInputElement>, row: number, col : number) => {
+    const createArrB = async(count : number)=>{
+        await setTableArrB([]);
+        await setArrB(new Array(Number(count)).fill(0));
+        console.log(arrB);
+        if(count > 0 && count <= 10){
+            setTimeout(() => {
+                const tempArr = [];
+                for (let i = 0; i < count; i++) {
+                    tempArr.push(<input type="number" className='w-[70px] h-[70px] text-center rounded-md' key={i} placeholder={`b${i+1}`} onInput={(event : React.ChangeEvent<HTMLInputElement> ) => onInputArrB(event, i)}/>);
+                }
+                setTableArrB(tempArr);
+            }, 0.001);
+        }
+    
+    }
+    
+    const onInputArrB = (event: React.ChangeEvent<HTMLInputElement>, row: number) => {
         if(event.target.value){
             const value : number = parseFloat(event.target.value);
-            setMatrix(v => {
+            setArrB(v => {
+                const newArrB = [...v];
+                newArrB[row] = value;
+                return newArrB;
+            });
+        }else{
+            setArrB(v => {
+                const newArrB = [...v];
+                newArrB[row] = 0;
+                return newArrB;
+            });
+        }
+    };
+
+    const onInputMartrixA = (event: React.ChangeEvent<HTMLInputElement>, row: number, col : number) => {
+        if(event.target.value){
+            const value : number = parseFloat(event.target.value);
+            setMatrixA(v => {
                 const newMatrixValues = v.map(row => [...row]);
                 newMatrixValues[row][col] = value;
                 return newMatrixValues;
             });
         }else{
-            setMatrix(v => {
+            setMatrixA(v => {
                 const newMatrixValues = v.map(row => [...row]);
                 newMatrixValues[row][col] = 0;
                 return newMatrixValues;
@@ -48,17 +100,33 @@ function TableMatrix(props : {count : number}) {
     };
 
     useEffect(() => {
-            createTableMatrix(props.count);
+            createTableMatrixA(props.count);
+            createArrX(props.count);
+            createArrB(props.count);
     }, [props.count]);
 
     useEffect(() => {
-        console.log(matrix);
-    }, [matrix]);
+        console.log(matrixA, arrB);
+    }, [matrixA,arrB]);
 
   return (
-    <div>
-        <div className='p-3 gap-3 rounded-md'>
-            {Tablematrix}
+    <div className='flex gap-1 p-3 justify-center items-center'>
+        <div className='gap-3 rounded-md'>
+            <span><BlockMath math={`\\begin{bmatrix} A \\end{bmatrix}`}/></span>
+            {TablematrixA}
+        </div>
+        <div className='gap-3 rounded-md ml-2'>
+            <span><BlockMath math={`\\{ X \\}`}/></span>
+            <div className='flex flex-col gap-2'>
+            {TableArrX}
+            </div>
+        </div>
+        <span className='text-2xl mr-3 ml-3'> = </span>
+        <div className='gap-3 rounded-md'>
+            <span><BlockMath math={`\\{ B \\}`}/></span>
+            <div className='flex flex-col gap-2'>
+            {TableArrB}
+            </div>
         </div>
     </div>
   )
