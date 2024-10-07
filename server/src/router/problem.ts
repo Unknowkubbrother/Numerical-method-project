a237cfa7786ae30f3d25070f327f77f6285cd7f1
Natchkung <natchnon654321@gmail.com>
2024-10-08 01:34:02 +0700

    fix deploy server

--- server/src/router/problem.ts
+++ server/src/router/problem.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import {problemCreate, problemGetByITS,problemGetByType,problemGetAll} from '../controllers/problem';
 
 export default (router: express.Router) => {
-    // router.post('/auth/register', register)
     router.post('/problem/create', problemCreate)
     router.post('/problem/getByITS', problemGetByITS) // input type solution
     router.post('/problem/getByType', problemGetByType)
