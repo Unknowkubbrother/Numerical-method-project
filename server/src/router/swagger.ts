9b1f1fcc28c1e651235af3c51d308019adeb25d6
Natchkung <natchnon654321@gmail.com>
2024-10-16 00:50:01 +0700

    test1

--- server/src/router/swagger.ts
+++ server/src/router/swagger.ts
@@ -4,10 +4,10 @@ import swaggerDocument from '../swagger.json';
 
 var options = {
     explorer: false,
-    customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
-    
 };
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 export default (router: express.Router) => {
-    router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+    router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
 }
