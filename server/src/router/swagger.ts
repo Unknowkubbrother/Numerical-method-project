3ae10308409eae753b2c34c5fa2204cf3a2cd738
Natchkung <natchnon654321@gmail.com>
2024-10-16 01:05:24 +0700

    test11

--- server/src/router/swagger.ts
+++ server/src/router/swagger.ts
@@ -4,15 +4,12 @@ import swaggerDocument from '../swagger.json';
 
 var options = {
     explorer: false,
+    customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.css",
+    customJsUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-bundle.js",
+    customJsUrlPreset: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-standalone-preset.js",
 };
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
 export default (router: express.Router) => {
-    router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
-        customCss:
-            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: start; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-        customCssUrl: CSS_URL,
-    }
-    ));
+    router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options));
 }
