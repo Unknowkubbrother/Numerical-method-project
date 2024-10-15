cfadfbb881f0dfc8103152285583ff8a84de57d4
Natchkung <natchnon654321@gmail.com>
2024-10-16 01:08:49 +0700

    test

--- server/src/router/swagger.ts
+++ server/src/router/swagger.ts
@@ -1,15 +1,11 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger.json';
+import * as swaggerDocument from '../swagger.json';
 
-var options = {
+const options = {
     explorer: false,
-    customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.css",
-    customJsUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-bundle.js",
-    customJsUrlPreset: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-standalone-preset.js",
 };
 
-
 export default (router: express.Router) => {
-    router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options));
+    router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 }
