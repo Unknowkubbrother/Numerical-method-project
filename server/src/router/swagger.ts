24d8d24f274126ca2366a1dc8b9ae805799dcf7a
Natchkung <natchnon654321@gmail.com>
2024-10-16 01:00:06 +0700

    test

--- server/src/router/swagger.ts
+++ server/src/router/swagger.ts
@@ -11,7 +11,7 @@ const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger
 export default (router: express.Router) => {
     router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
         customCss:
-            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: start; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
         customCssUrl: CSS_URL,
     }
     ));
