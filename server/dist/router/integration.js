1884f863baf89d32e4faf4ac1b5e7b20742b8105
Natchkung <natchnon654321@gmail.com>
2024-10-23 15:56:56 +0700

    fix bug

--- server/dist/router/integration.js
+++ server/dist/router/integration.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const integration_1 = require("../controllers/integration");
 exports.default = (router) => {
-    router.post('/interpolation/singtrapezoidal', integration_1.SingTrapezoidal);
+    router.post('/interpolation/singtrapezoidal', integration_1.Trapezoidal);
     router.post('/interpolation/compositetrapezoidal', integration_1.CompositeTrapezoidal);
     router.post('/interpolation/simpson', integration_1.Simpson);
     router.post('/interpolation/compositesimpson', integration_1.CompositeSimpson);
--- server/src/router/integration.ts
+++ server/src/router/integration.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import { SingTrapezoidal, CompositeTrapezoidal, Simpson, CompositeSimpson} from '../controllers/integration';
+import { Trapezoidal, CompositeTrapezoidal, Simpson, CompositeSimpson} from '../controllers/integration';
 export default (router: express.Router) => {
-    router.post('/interpolation/singtrapezoidal', SingTrapezoidal)
+    router.post('/interpolation/singtrapezoidal', Trapezoidal)
     router.post('/interpolation/compositetrapezoidal', CompositeTrapezoidal)
     router.post('/interpolation/simpson', Simpson)
     router.post('/interpolation/compositesimpson', CompositeSimpson)
